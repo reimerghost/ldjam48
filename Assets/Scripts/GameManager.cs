f1a216a44b3a5bdb9750499b022f89b248147952
Reimer Chamalé <rchamale10@gmail.com>
2021-04-24 22:34:16 -0600

    GM

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
